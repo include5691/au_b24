f42a77c35a241b995c6faf1c272f29f736629408
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-07 12:24:02 +0500

    feat[_requests]: added _get_url method

---
+++ _requests/_url.py
@@ -0,0 +1,20 @@
+import os
+
+def _get_url(method: str) -> str | None:
+    """
+    Get url by method 
+    
+    `method`: rest api b24 method. Method examples: *crm.lead.get*, *user.get*
+    """
+    if not method or not isinstance(method, str):
+        return None
+    if "." not in method:
+        return None
+    scope: str = method.split(".")[0]
+    usertoken = os.getenv(f"{scope.upper()}_USERTOKEN")
+    if not usertoken:
+        return None
+    bitrix_id = os.getenv("BITRIX_ID")
+    if not bitrix_id:
+        return None
+    return f"https://{bitrix_id}.bitrix24.ru/rest{usertoken}/{method}.json"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='au_b24',
+    version='0.1.0',
+    author='Andrey Pshenitsyn',
+    description='Bitrix24 rest api automatization library',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'cachetools',
+        "python-dotenv"
+    ],
+)
