c11ae3a7a2f3b9eb6a728a0ccb35eaa9cb5925af
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-11 22:48:51 +0500

    docs[funcs/opelines/transfer]: added note about 400 status code error

--- au_b24/funcs/openlines/transfer.py
+++ au_b24/funcs/openlines/transfer.py
@@ -1,6 +1,12 @@
 from ..._requests import post
 
 def transfer_openline(ol_id: int | str, user_id: int | str) -> bool | None:
-    """Transfer openline to another user"""
+    """
+    Transfer openline to providen users
+    
+    Note
+    ----
+    May occurs unexplained error with 400 status code - like there is not righ to transfer foreign chat 
+    """
     result = post("imopenlines.operator.transfer", {"CHAT_ID": ol_id, "TRANSFER_ID": user_id})
     return result if result else None
