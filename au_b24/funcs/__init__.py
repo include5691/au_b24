9c7df8e256e60357fc0f22e377717bd626725ca0
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-11 13:24:49 +0500

    feat[funcs]: added add_comment func

--- au_b24/funcs/__init__.py
+++ au_b24/funcs/__init__.py
@@ -1,2 +1,3 @@
 from .leads import *
 from .openlines import *
+from .comments import add_comment
---
+++ au_b24/funcs/comments.py
@@ -0,0 +1,9 @@
+from typing import Literal
+from .._requests import post
+
+def add_comment(entity_id: int | str, entity_type: Literal["lead", "deal"], comment: str) -> int | None:
+    """
+    Add comment to entity
+    Returns comment id
+    """
+    return post("crm.timeline.comment.add", {"fields": {"ENTITY_ID": entity_id, "ENTITY_TYPE": entity_type, "COMMENT": comment}})
