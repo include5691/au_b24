ee772e3a80c5151e619635c294b6af1f540bbf44
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-12 18:49:18 +0500

    feat[funcs/notifications]: added notify_user func

--- au_b24/funcs/__init__.py
+++ au_b24/funcs/__init__.py
@@ -2,3 +2,4 @@ from .users import *
 from .leads import *
 from .openlines import *
 from .comments import add_comment
+from .notifications import notify_user
---
+++ au_b24/funcs/notifications.py
@@ -0,0 +1,5 @@
+from .._requests import post
+
+def notify_user(user_id: int | str, message: str) -> int | None:
+    """Add notification to user. Returns notification ID"""
+    return post("im.notify.personal.add", {"USER_ID": user_id, "MESSAGE": message})
