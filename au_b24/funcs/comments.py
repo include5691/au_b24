4408cc0c39ae8945c0522907d238f462fa3b59da
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-13 20:31:45 +0500

    chore[funcs/comments]: rename comment arg to text

--- au_b24/funcs/comments.py
+++ au_b24/funcs/comments.py
@@ -1,9 +1,9 @@
 from typing import Literal
 from ..reqs import post
 
-def add_comment(entity_id: int | str, entity_type: Literal["lead", "deal"], comment: str) -> int | None:
+def add_comment(entity_id: int | str, entity_type: Literal["lead", "deal"], text: str) -> int | None:
     """
     Add comment to entity
     Returns comment id
     """
-    return post("crm.timeline.comment.add", {"fields": {"ENTITY_ID": entity_id, "ENTITY_TYPE": entity_type, "COMMENT": comment}})
+    return post("crm.timeline.comment.add", {"fields": {"ENTITY_ID": entity_id, "ENTITY_TYPE": entity_type, "COMMENT": text}})
