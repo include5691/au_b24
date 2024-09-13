8e050d82107fa54b3df74fc9fdd4507e6f649da4
Andrey Pshenitsyn <include5691@gmail.com>
2024-09-13 15:11:29 +0500

    docs[funcs/opelines/transfer]: update docstrings

--- au_b24/funcs/openlines/transfer.py
+++ au_b24/funcs/openlines/transfer.py
@@ -1,14 +1,7 @@
 from ...reqs import post, get
 
 def intercept_openline(ol_id: int | str) -> bool | None:
-    """
-    Intercept openline
-    
-    Note
-    ----
-    Needs to transfer dial if get_openlines() returns empty list but get_last_openline_id() returns some id.
-    Intersept openline, then transfer it to another user
-    """
+    """Intercept openline from another user"""
     result = get("imopenlines.session.intercept", {"CHAT_ID": ol_id})
     return result if result else None
 
@@ -18,7 +11,7 @@ def transfer_openline(ol_id: int | str, user_id: int | str) -> bool | None:
     
     Note
     ----
-    May occurs unexplained error with 400 status code - like there is not righ to transfer foreign chat 
+    May occurs unexplained error with 400 status code (update: only for transfering on yourself)
     """
     result = post("imopenlines.operator.transfer", {"CHAT_ID": ol_id, "TRANSFER_ID": user_id})
     return result if result else None
