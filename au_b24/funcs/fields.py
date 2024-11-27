5680f80394cf3d27f4380cad226e94cf0f0205a7
Andrey Pshenitsyn <include5691@gmail.com>
2024-11-27 21:19:41 +0500

    feat[funcs/fields]: add extract_enumerated_field_value func

---
+++ au_b24/funcs/fields.py
@@ -0,0 +1,19 @@
+from typing import Literal
+from ..reqs import post
+
+def extract_enumerated_field_value(entity_type: Literal["lead", "deal"], field_id: str, value: str | int):
+    """Extract enumerated field value by its id and value"""
+    if entity_type not in ["lead", "deal"]:
+        raise ValueError("entity_type must be 'lead' or 'deal'")
+    fields = post(f"crm.{entity_type}.fields", {})
+    if not fields:
+        return None
+    field = fields.get(field_id)
+    if not field:
+        return None
+    if field.get("type") != "enumeration":
+        raise ValueError("Field is not enumeration")
+    for item in field.get("items"):
+        if item.get("ID") == str(value):
+            return item.get("VALUE")
+    return None
