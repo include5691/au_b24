b42cf967b60006e33b599e3a214283eb2c5a866e
Andrey Pshenitsyn <include5691@gmail.com>
2024-11-28 12:41:22 +0500

    feat[funcs/fields]: add extract_enumerated_smart_field_value func

--- au_b24/funcs/__init__.py
+++ au_b24/funcs/__init__.py
@@ -9,4 +9,4 @@ from .notifications import notify_user
 from .statuses import get_sources, get_statuses
 from .dates import to_unix_time
 from .exceptions import StopParsing
-from .fields import extract_enumerated_field_value
+from .fields import extract_enumerated_field_value, extract_enumerated_smart_field_value
