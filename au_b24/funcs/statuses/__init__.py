8d0ffda0365359f1127aa178f5d0ca44c9ebe05d
Andrey Pshenitsyn <include5691@gmail.com>
2024-12-17 15:46:18 +0500

    feat[funcs/statuses]: add get_stage_history func

--- au_b24/funcs/statuses/__init__.py
+++ au_b24/funcs/statuses/__init__.py
@@ -1,3 +1,4 @@
 from .sources import get_sources
 from .statuses import get_statuses
 from .stages import get_stages
+from .history import get_stage_history
---
+++ au_b24/funcs/statuses/history.py
@@ -0,0 +1,23 @@
+from typing import Literal
+from ...reqs import post
+
+def get_stage_history(entity_type: Literal["lead", "deal"], entity_id: str | int, select: list = ["*"], order: Literal["ASC", "DESC"] = "ASC") -> list[dict] | None:
+    """
+    Get status or stage history
+    
+    :param select: support ID, TYPE_ID, TYPE_ID, CREATED_TIME, STATUS_SEMANTIC_ID and STATUS_ID fields
+    """
+    if entity_type == "lead":
+        entity_type_id = 1
+    elif entity_type == "deal":
+        entity_type_id = 2
+    else:
+        return None
+    response = post("crm.stagehistory.list", {"entityTypeId": entity_type_id, "order": {"ID": order}, "filter": {"OWNER_ID": entity_id}, "select": select, "start": "-1"})
+    if not response or not isinstance(response, dict):
+        return None
+    items =  response.get("items")
+    if not items or not isinstance(items, list):
+        return None
+    result = [item for item in items if item and isinstance(item, dict)]
+    return result if result else None
