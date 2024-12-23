f82ed5563609f902226d6647c202f1ad578c7d28
Andrey Pshenitsyn <include5691@gmail.com>
2024-12-23 19:19:14 +0500

    chore[funcs/(deals, leads)/parse]: remove deprecated warngings for parse funcs

--- au_b24/funcs/deals/parse.py
+++ au_b24/funcs/deals/parse.py
@@ -1,7 +1,5 @@
 from typing import Callable
-from typing_extensions import deprecated
 from ..entities import parse_entities
 
-@deprecated("Use parse_entities instead")
 def parse_deals(fn: Callable):
     return parse_entities("deal", fn)
