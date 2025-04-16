a47a9704ba7e4a4269a9feca2cc49dc8330b945c
Andrey Pshenitsyn <include5691@gmail.com>
2025-04-17 00:35:40 +0500

    feat(funcs/contacts): add parse_contacts decorator

--- au_b24/funcs/contacts/__init__.py
+++ au_b24/funcs/contacts/__init__.py
@@ -2,3 +2,4 @@ from .delete import delete_contact
 from .get import get_contact, get_contacts
 from .phone import get_phone_by_contact
 from .update import update_contact
+from .parse import parse_contacts
