13cbf817aa834656882ab552bdc5fc7b4654bddf
Andrey Pshenitsyn <include5691@gmail.com>
2025-07-05 22:15:56 +0500

    feat(funcs/entities/parse): add allow_id_filter arg and custom id filtering

--- au_b24/aiofuncs/entities/parse.py
+++ au_b24/aiofuncs/entities/parse.py
@@ -6,13 +6,14 @@ from ..exceptions import StopParsing
 def parse_entities(entity_type: Literal["lead", "deal", "contact"], fn: Callable):
     if entity_type not in ["lead", "deal", "contact"]:
         raise ValueError("Entity type incorrect")
-    def wrapper(filters: dict, select: list, order: Literal["ASC", "DESC"] = "ASC", **kwargs):
+    def wrapper(filters: dict, select: list, order: Literal["ASC", "DESC"] = "ASC", allow_id_filter: bool = False, **kwargs):
         """
         :param filters: filters by fields, allowing '<', '>' and '!' logical symbols, and grouping by []. ID filtering allowing too
         :param select: list of selected fields. Passing '*' will select all fields
         :param order: sorting by id
         """
         async def async_wrapper():
+            nonlocal order, allow_id_filter
             if not isinstance(filters, dict):
                 raise ValueError("Filters and select must be a dict")
             if not select:
@@ -21,15 +22,29 @@ def parse_entities(entity_type: Literal["lead", "deal", "contact"], fn: Callable
                 raise ValueError("Select must be a list")
             if order not in ("ASC", "DESC"):
                 raise ValueError("Order must be 'ASC' or 'DESC'")
-            if {">ID", "<ID", ">=ID", "<=ID"} & set(filters):
-                raise ValueError("ID filtering can't be used with '<', '>', '>=' and '<='")
-            filters_copy = filters.copy()
-            if order == "ASC":
-                filters_copy.update({">ID": 0})
-                id_key = ">ID"
+            id_keys = list({">ID", "<ID", ">=ID", "<=ID"} & set(filters))
+            if id_keys:
+                if not allow_id_filter:
+                    raise ValueError("ID filtering can't be used with '<', '>', '>=' and '<='")
+                allow_id_filter = True
+                for key in id_keys[1:]:
+                    del filters[key]
+                id_key = id_keys.pop()
+                if "<" in id_key:
+                    order = "DESC"
+                elif ">" in id_key:
+                    order = "ASC"
             else:
-                filters_copy.update({"<ID": 2**32})
-                id_key = "<ID"
+                if allow_id_filter:
+                    allow_id_filter = False
+            filters_copy = filters.copy()
+            if not allow_id_filter:
+                if order == "ASC":
+                    filters_copy.update({">ID": 0})
+                    id_key = ">ID"
+                else:
+                    filters_copy.update({"<ID": 2**32})
+                    id_key = "<ID"
             counter = 0
             while True:
                 entities: list[dict] | None = await post(f"crm.{entity_type}.list", {"filter": filters_copy, "select": select, "order": {"ID": order}, "start": -1})
